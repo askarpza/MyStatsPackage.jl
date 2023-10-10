e8ba10809c863e6bc6fe7eea5d3481e39785822f
Zahra Askarpour <Zahra.Askarpour@mathematik.uni-stuttgart.de>
2023-10-10 12:08:01 +0200

    first commit

---
+++ src/MyStatsPackage.jl
@@ -0,0 +1,12 @@
+module MyStatsPackage
+
+include("../../../firststeps/statistic_functions.jl")
+
+export rse_mean
+export rse_std
+export rse_sum
+export rse_tstat
+
+end # module MyStatsPackage
+
+print(askarpza)
